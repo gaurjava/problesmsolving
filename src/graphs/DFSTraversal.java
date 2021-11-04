4ab7c62af31638c366015679a59b4de8f4dd9402
Bhupesh Sharma <bhupesh>
2021-11-05 00:09:43 +0530

    dfs_bfs

--- src/graphs/DFSTraversal.java
+++ src/graphs/DFSTraversal.java
@@ -1,9 +1,34 @@
 package graphs;
 
+import java.util.List;
+
 public class DFSTraversal {
 
 	public static void main(String[] args) {
-
+		dfstraversal(Graph.getGraph());
 	}
 
+	public static void dfstraversal(Graph graph) {
+		int n = graph.adj.size();
+		boolean visited[] = new boolean[n];
+		
+		for(int i=0;i<n;i++) {
+			if(!visited[i]) {
+				dfsroutine(graph,visited, i);
+			}
+		}
+	}
+	
+	public static void dfsroutine(Graph graph,boolean[] visited,int node) {
+		System.out.print(node +" ");
+		visited[node] =true;
+	
+		List<Integer> ads = graph.adj.get(node);
+		for(int i=0;i<ads.size();i++) {
+			if(!visited[ads.get(i)]) {
+				dfsroutine(graph, visited, ads.get(i));
+			}
+		}
+		
+	}
 }
